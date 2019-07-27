29dc77c7222a5b8a783e37d992a9155f059b52e1
Kent Yang <myang6@hdfgroup.org>
2019-07-26 19:20:41 -0500

    HK-465, add the hacked FFTypeFactory code. Can compile.

---
+++ modules/freeform_handler/FFTypeFactory.cc
@@ -0,0 +1,183 @@
+
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2005 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+
+#include <string>
+#include <BESError.h>
+#include <BESDebug.h>
+
+#include "FFByte.h"
+#include "FFInt16.h"
+#include "FFUInt16.h"
+#include "FFInt32.h"
+#include "FFUInt32.h"
+#include "FFFloat32.h"
+#include "FFFloat64.h"
+#include "FFStr.h"
+#include "FFUrl.h"
+#include "FFArray.h"
+#include "FFStructure.h"
+#include "FFSequence.h"
+#include "FFGrid.h"
+
+#include "BaseTypeFactory.h"
+#include "FFTypeFactory.h"
+
+#include "debug.h"
+
+using namespace libdap;
+using namespace std;
+
+BaseType *FFTypeFactory::NewVariable(Type t, const string &name) const
+{
+	switch (t) {
+	case dods_byte_c:
+		return NewByte(name);
+	//case dods_char_c:
+	//	return NewChar(name);
+
+	//case dods_uint8_c:
+	//	return NewUInt8(name);
+	//case dods_int8_c:
+	//	return NewInt8(name);
+
+	case dods_int16_c:
+		return NewInt16(name);
+	case dods_uint16_c:
+		return NewUInt16(name);
+	case dods_int32_c:
+		return NewInt32(name);
+	case dods_uint32_c:
+		return NewUInt32(name);
+
+	//case dods_int64_c:
+	//	return NewInt64(name);
+	//case dods_uint64_c:
+	//	return NewUInt64(name);
+
+	case dods_float32_c:
+		return NewFloat32(name);
+	case dods_float64_c:
+		return NewFloat64(name);
+
+	case dods_str_c:
+		return NewStr(name);
+	case dods_url_c:
+		return NewUrl(name);
+
+	case dods_array_c:
+		return NewArray(name);
+
+	case dods_structure_c:
+		return NewStructure(name);
+
+	case dods_sequence_c:
+		return NewSequence(name);
+
+	default:
+		throw BESError("Unimplemented type in DAP2.", BES_INTERNAL_ERROR, __FILE__, __LINE__);
+	}
+}
+
+
+Byte *
+FFTypeFactory::NewByte(const string &n ) const 
+{ 
+    return new FFByte(n,"");
+}
+
+Int16 *
+FFTypeFactory::NewInt16(const string &n ) const 
+{ 
+    return new FFInt16(n,""); 
+}
+
+UInt16 *
+FFTypeFactory::NewUInt16(const string &n ) const 
+{ 
+    return new FFUInt16(n,"");
+}
+
+Int32 *
+FFTypeFactory::NewInt32(const string &n ) const 
+{ 
+    DBG(cerr << "Inside FFTypeFactory::NewInt32" << endl);
+    return new FFInt32(n,"");
+}
+
+UInt32 *
+FFTypeFactory::NewUInt32(const string &n ) const 
+{ 
+    return new FFUInt32(n,"");
+}
+
+Float32 *
+FFTypeFactory::NewFloat32(const string &n ) const 
+{ 
+    return new FFFloat32(n,"");
+}
+
+Float64 *
+FFTypeFactory::NewFloat64(const string &n ) const 
+{ 
+    return new FFFloat64(n,"");
+}
+
+Str *
+FFTypeFactory::NewStr(const string &n ) const 
+{ 
+    return new FFStr(n,"");
+}
+
+Url *
+FFTypeFactory::NewUrl(const string &n ) const 
+{ 
+    return new FFUrl(n,"");
+}
+
+Array *
+FFTypeFactory::NewArray(const string &n , BaseType *v) const 
+{ 
+    return new FFArray(n,"",v,"");
+}
+
+Structure *
+FFTypeFactory::NewStructure(const string &n ) const 
+{ 
+    return new FFStructure(n,"");
+}
+
+Sequence *
+FFTypeFactory::NewSequence(const string &n ) const 
+{
+    DBG(cerr << "Inside FFTypeFactory::NewSequence" << endl);
+    return new FFSequence(n,"","");
+}
+
+Grid *
+FFTypeFactory::NewGrid(const string &n ) const 
+{ 
+    return new FFGrid(n,"");
+}
---
+++ modules/freeform_handler/FFTypeFactory.h
@@ -0,0 +1,70 @@
+
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// Copyright (c) 2005 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your
+// option) any later version.
+// 
+// This is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#ifndef ff_factory_h
+#define ff_factory_h
+
+#include <string>
+
+#include "BaseTypeFactory.h"
+
+class FFByte;
+class FFInt16;
+class FFUInt16;
+class FFInt32;
+class FFUInt32;
+class FFFloat32;
+class FFFloat64;
+class FFStr;
+class FFUrl;
+class FFArray;
+class FFStructure;
+class FFSequence;
+class FFGrid;
+
+/** A factory for the FFByte, ..., FFGrid types.
+    @author James Gallagher */
+class FFTypeFactory : public libdap::BaseTypeFactory {
+public:
+    FFTypeFactory() {} 
+    virtual ~FFTypeFactory() {}
+
+    virtual libdap::BaseType *NewVariable(libdap::Type t, const std::string &name) const;
+    virtual libdap::Byte *NewByte(const std::string &n = "") const;
+    virtual libdap::Int16 *NewInt16(const std::string &n = "") const;
+    virtual libdap::UInt16 *NewUInt16(const std::string &n = "") const;
+    virtual libdap::Int32 *NewInt32(const std::string &n = "") const;
+    virtual libdap::UInt32 *NewUInt32(const std::string &n = "") const;
+    virtual libdap::Float32 *NewFloat32(const std::string &n = "") const;
+    virtual libdap::Float64 *NewFloat64(const std::string &n = "") const;
+
+    virtual libdap::Str *NewStr(const std::string &n = "") const;
+    virtual libdap::Url *NewUrl(const std::string &n = "") const;
+
+    virtual libdap::Array *NewArray(const std::string &n = "", libdap::BaseType *v = 0) const;
+    virtual libdap::Structure *NewStructure(const std::string &n = "") const;
+    virtual libdap::Sequence *NewSequence(const std::string &n = "") const;
+    virtual libdap::Grid *NewGrid(const std::string &n = "") const;
+    //virtual libdap::D4Sequence *NewD4Sequence(const std::string &n="") const;
+};
+
+#endif // ff_factory_h
