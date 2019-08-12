a2c68086338da18c1ec152bc1f8aeb473a8b4d56
Kent Yang <myang6@hdfgroup.org>
2019-08-12 18:08:17 -0500

    HK-478, create a sample test script for data access.

--- modules/netcdf_handler/test_dds.sh
+++ modules/netcdf_handler/test_dds.sh
@@ -7,14 +7,15 @@
 #besstandalone -d"cerr,ff" -c tests/bes.conf -i tests/ff/dbl_data.bin.data.bescmd | getdap -M -
 #besstandalone -c tests/bes.conf -i tests/ff/dbl_data.bin.data.bescmd >dbl_data.bin.dods
 #valgrind besstandalone -c tests/bes.conf -i tests/ff/dbl_data.bin.data.bescmd | getdap -M -
-besstandalone -c tests/bes.conf -i tests/ff/dbl_data.bin.data.bescmd | getdap -M -
-besstandalone -c tests/bes.conf -i tests/ff/dbl_data3.bin.data.bescmd | getdap -M -
+besstandalone -c tests/bes.conf -i tests/nc/fnoc1.nc.3.bescmd | getdap -M -
+besstandalone -c tests/bes.conf -i tests/nc/fnoc1.nc.2.bescmd 
+#besstandalone -c tests/bes.conf -i tests/ff/dbl_data3.bin.data.bescmd | getdap -M -
 #besstandalone -d"cerr,ff" -c tests/bes.conf -i tests/ff/dbl_data3.bin.data.bescmd | getdap -M -
 #valgrind besstandalone -c tests/bes.conf -i tests/ff/dbl_data3.bin.data.bescmd | getdap -M -
 #valgrind --leak-check=full besstandalone -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M -
 #valgrind --leak-check=full besstandalone -c tests/bes.conf -i tests/ff/test5.dab.das.bescmd
 #valgrind besstandalone -c tests/bes.conf -i tests/ff/test5.dab.das.bescmd
-besstandalone -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M -
+#besstandalone -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M -
 #besstandalone -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M -
 #valgrind besstandalone -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M - 
 #besstandalone -d"cerr,all" -c tests/bes.conf -i tests/ff/test5.dab.data.bescmd | getdap -M - >&tmp
