2fb44688f9503eae0618f4f245200ec2d0cabf73
Muqun Yang <myang6@hdfgroup.org>
2019-08-13 17:23:57 -0500

    HK-478, add airs testing bescmd files

--- modules/netcdf_handler/test_dds.sh
+++ modules/netcdf_handler/test_dds.sh
@@ -1,8 +1,10 @@
 #besstandalone -d"cerr,all" -c tests/bes.conf -i tests/nc/nc4_nc_classic_compressed.nc.1.bescmd 
 #besstandalone -d"cerr,all" -c tests/bes.conf -i tests/nc/fnoc1.nc.3.bescmd | getdap -M - 
-besstandalone -c tests/bes.conf -i tests/nc/fnoc1.nc.3.bescmd | getdap -M - 
+besstandalone -c tests/bes.conf -i tests/nc/airs.data.bescmd | getdap -M -
+besstandalone -c tests/bes.conf -i tests/nc/airs.dds.bescmd 
+#besstandalone -c tests/bes.conf -i tests/nc/fnoc1.nc.3.bescmd | getdap -M - 
 #besstandalone -c tests/bes.conf -i tests/nc/nc4_nc_classic_compressed.nc.3.bescmd | getdap -M -
-besstandalone -c tests/bes.conf -i tests/nc/nc4_nc_classic_compressed.nc.3.bescmd | getdap -M -
+#besstandalone -c tests/bes.conf -i tests/nc/nc4_nc_classic_compressed.nc.3.bescmd | getdap -M -
 #besstandalone -d"cerr,all" -c tests/bes.conf -i tests/nc/nc4_nc_classic_compressed.nc.3.bescmd | getdap -M -
 #besstandalone -c tests/bes.conf -i tests/nc/fnoc1.nc.2.bescmd 
 
